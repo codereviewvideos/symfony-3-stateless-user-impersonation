2954f9cb1c90eed5ee39fe3faab93d0a2fa8b2ac
Chris <chris@codereviewvideos.com>
2017-10-30 16:30:53 +0000

    reset profile get action to basic

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -6,12 +6,7 @@ use FOS\RestBundle\Controller\Annotations;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\Controller\Annotations\RouteResource;
-use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Security\Core\Role\SwitchUserRole;
 
 /**
  * @RouteResource("profile", pluralize=false)
@@ -19,32 +14,19 @@ use Symfony\Component\Security\Core\Role\SwitchUserRole;
 class ProfileController extends FOSRestController implements ClassResourceInterface
 {
     /**
-     * @Annotations\Get("/profile/{user}")
-     * @ParamConverter("user", class="AppBundle:User")
+     * @Annotations\Get("/profile")
      *
      * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
      */
-    public function getAction(
-        AuthorizationCheckerInterface $authorizationChecker,
-        TokenStorageInterface $tokenStorage,
-        UserInterface $user
-    )
+    public function getAction()
     {
-//        if ($authorizationChecker->isGranted('ROLE_PREVIOUS_ADMIN')) {
-//            foreach ($tokenStorage->getToken()->getRoles() as $role) {
-//                if ($role instanceof SwitchUserRole) {
-//                    return $user;
-//                }
-//            }
-//        }
-
-        if ($user === $this->getUser()) {
-            return $user;
+        if (null === $this->getUser()) {
+            return new JsonResponse(
+                'Access denied',
+                JsonResponse::HTTP_FORBIDDEN
+            );
         }
 
-        return new JsonResponse(
-            'Access denied',
-            JsonResponse::HTTP_FORBIDDEN
-        );
+        return $this->getUser();
     }
 }
