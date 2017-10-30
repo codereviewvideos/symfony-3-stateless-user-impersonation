a1455815fe5adce98988f6cc1a3368baf73fb22b
Chris <chris@codereviewvideos.com>
2017-10-30 16:05:16 +0000

    clean up profile controller

--- src/AppBundle/Controller/ProfileController.php
+++ src/AppBundle/Controller/ProfileController.php
@@ -9,9 +9,6 @@ use FOS\RestBundle\Controller\Annotations\RouteResource;
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use Symfony\Component\Security\Core\Role\SwitchUserRole;
 
 /**
  * @RouteResource("profile", pluralize=false)
@@ -19,25 +16,15 @@ use Symfony\Component\Security\Core\Role\SwitchUserRole;
 class ProfileController extends FOSRestController implements ClassResourceInterface
 {
     /**
-     * @Annotations\Get("/profile/{user}")
+     * @Annotations\Get("/profile")
      * @ParamConverter("user", class="AppBundle:User")
      *
      * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
      */
     public function getAction(
-        AuthorizationCheckerInterface $authorizationChecker,
-        TokenStorageInterface $tokenStorage,
         UserInterface $user
     )
     {
-//        if ($authorizationChecker->isGranted('ROLE_PREVIOUS_ADMIN')) {
-//            foreach ($tokenStorage->getToken()->getRoles() as $role) {
-//                if ($role instanceof SwitchUserRole) {
-//                    return $user;
-//                }
-//            }
-//        }
-
         if ($user === $this->getUser()) {
             return $user;
         }
